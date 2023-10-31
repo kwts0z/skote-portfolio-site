76a9b922317e660e7b77dda29492816d6f425053
SoundSkote <98184441+SoundSkote@users.noreply.github.com>
2023-10-31 01:58:53 +0000

    Update implementationReelsSection.js
    bg-white abandoned hospital

--- src/components/implementationReelsSection.js
+++ src/components/implementationReelsSection.js
@@ -40,10 +40,10 @@ function ImplementationReels() {
         <div className='flex flex-col w-full items-start lg:flex-row lg:space-x-5'>
           <div className='flex flex-col w-full'>
             <ReactPlayer width='100%' height='100%' style={style} url='https://vimeo.com/870703436' controls={true}/>
-            <div className='flex flex-col space-y-5 text-black bg-white pt-5 z-10 text-xl xl:text-2xl'>
+            <div className='flex flex-col space-y-5 text-black pt-5 z-10 text-xl xl:text-2xl'>
               <h1 className='font-extrabold text-3xl 2xl:text-4xl'>Abandoned Hospital Demo</h1>
-              <h2 className='font-bold'>Sound Design and UE 5 Implementation</h2>
-              <p className='text-left pb-5'>Abandoned Hospital is a demo level that I used to practice and share my UE 5 implementation skills. This video shows the gameplay of the game. I've added all the sounds and interactions happening in the game from the environment to collecting items. </p>
+              <h2 className='font-bold bg-white'>Sound Design and UE 5 Implementation</h2>
+              <p className='text-left bg-white pb-5'>Abandoned Hospital is a demo level that I used to practice and share my UE 5 implementation skills. This video shows the gameplay of the game. I've added all the sounds and interactions happening in the game from the environment to collecting items. </p>
             </div>
           </div>
           <div className='flex flex-col w-full'>
